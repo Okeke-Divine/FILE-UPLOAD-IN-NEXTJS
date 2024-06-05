850177fd82a1ab8ccd253896c20e86fc2f282290
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-06-05 20:35:51 +0100

    Finalized the cloudinary upload

--- app/cloudinary/page.jsx
+++ app/cloudinary/page.jsx
@@ -1,5 +1,5 @@
+"use server";
 import { v2 as cloudinary } from "cloudinary";
-import React from "react";
 
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUDNAME,
@@ -10,25 +10,52 @@ cloudinary.config({
 const Cloudinary = () => {
   const uploadFile = async (formData) => {
     "use server";
-    console.log("create");
-    const file = formData.get("file");
-    console.table(file);
+    try {
+      console.log("create");
+      const file = formData.get("file");
+
+      const bytes = await file.arrayBuffer();
+      const buffer = new Uint8Array(bytes);
+      await new Promise((resolve, reject) => {
+        const uploaded_file = cloudinary.uploader
+          .upload_stream(
+            {
+              tags: ["file-upload-in-nextjs"],
+            },
+            function (error, result) {
+              if (error) {
+                console.log("Cloudinary error", error)
+                reject(error);
+                return;
+              }else{
+                console.log("Cloudinary Success")
+              }
+              resolve(result);
+            }
+          )
+          .end(buffer);
+          console.log(uploaded_file)
+      });
+    } catch (err) {
+      console.log("Error: ", err);
+    }
   };
 
   return (
     <>
       <div>
-        <form className="p-5 rounded border-dotted border-2">
-          <form action={uploadFile}>
-            <input type="file" name="file" id="file" />
-            <br />
-            <button
-              type="submit"
-              className="bg-black text-white rounded py-2 px-5 mt-2"
-            >
-              Upload
-            </button>
-          </form>
+        <form
+          className="p-5 rounded border-dotted border-2"
+          action={uploadFile}
+        >
+          <input type="file" name="file" id="file" />
+          <br />
+          <button
+            type="submit"
+            className="bg-black text-white rounded py-2 px-5 mt-2"
+          >
+            Upload
+          </button>
         </form>
       </div>
     </>
